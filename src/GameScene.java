3fd3ad88c145c8ede861b8e5ba42935c0042c222
eliyahu <elihaim82gmail.com>
2021-06-15 12:34:08 +0300

    Initial commit

---
+++ src/GameScene.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class GameScene extends JPanel {
+
+    public GameScene() {
+        this.setDoubleBuffered(true);
+    }
+}
---
+++ src/Paintable.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+
+public interface Paintable {
+    public void paint (Graphics graphics);
+    public void move (int direction);
+}
